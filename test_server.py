96eeaecb06aef649d21f909663403816f0f5168e
rockcutter <rockcutter414@gmail.com>
2023-12-14 02:31:39 +0900

    add python test udp server for ci test

---
+++ test_server.py
@@ -0,0 +1,20 @@
+import socket
+import sys
+
+server_addr = ('', 8254)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+sock.bind(server_addr)
+
+M_SIZE = 1024
+
+while True:
+    print("wait_for_msg", file=sys.stderr)
+    bytes_message, client_addr = sock.recvfrom(M_SIZE)
+    decoded_message = bytes_message.decode()
+    print(decoded_message, file=sys.stderr)
+    send_message = decoded_message + "recieved. v6"
+
+    sock.sendto(send_message.encode(), client_addr)
+
