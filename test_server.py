63affeb527055aa3b80df202eeabff211718530e
rockcutter <rockcutter414@gmail.com>
2023-12-16 00:55:19 +0900

    負荷書ける処理を追加j

--- test_server.py
+++ test_server.py
@@ -15,6 +15,10 @@ while True:
     decoded_message = bytes_message.decode()
     print(decoded_message, file=sys.stderr)
     send_message = decoded_message + "recieved. v6"
+    i = 0
+    while i < 100000000:
+        i = i + 1;
+
+    send_message = send_message + str(i)
 
     sock.sendto(send_message.encode(), client_addr)
-
